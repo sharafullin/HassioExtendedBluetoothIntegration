ade7a968456fe85f490925e3610c205add052cc9
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2020-05-08 09:10:57 +0200

    added climate component

---
+++ climate.py
@@ -0,0 +1,13 @@
+from bluetooth_hub_discovery import discover, configure
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Discover ha-rpi-bt-ext devices."""
+    devices = []
+
+    hubs = discover()    
+
+    mqtt_ip = config[CONF_MQTT][CONF_BROKER]
+
+    dev_configs = []
+    for hub in hubs:
+        dev_configs += configure(hub, mqtt_ip)
